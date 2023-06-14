b864826182c6c47379058e55d177c1b36149a38a
BJ Collins <bj@BJs-MacBook-Pro-2.local>
2023-06-13 18:41:09 -0600

    Fixed issue with date formats, yeesh

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,58 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import { useState, useEffect } from "react";
-
-const formatDate = (date) => {
-  const dateObject = new Date(date);
-  const day = dateObject.getDate();
-  const month = dateObject.getMonth() + 1;
-  const year = dateObject.getFullYear();
-  return `${year}-${month}-${day}`;
-};
+import { useState, useEffect } from 'react'
 
 async function getScoreboardData({ date }) {
-  let scoreboardData = await fetch(`/api/scoreboard/${date}`);
-  scoreboardData = await scoreboardData.json();
+  let scoreboardData = await fetch(`/api/scoreboard/${date}`)
+  scoreboardData = await scoreboardData.json()
+  console.log(
+    '🚀 ~ file: page.jsx:17 ~ getScoreboardData ~ scoreboardData:',
+    scoreboardData
+  )
+}
+
+function getToday() {
+  const unformattedDate = Intl.DateTimeFormat('en-US').format(new Date())
+  let month = unformattedDate.split('/')[0]
+  if (month.length === 1) {
+    month = `0${month}`
+  }
+  let day = unformattedDate.split('/')[1]
+  if (day.length === 1) {
+    day = `0${day}`
+  }
+  const year = unformattedDate.split('/')[2]
+  return `${year}-${month}-${day}`
+}
+
+function formatDate(date) {
+  const year = date.split('-')[0]
+  let month = date.split('-')[1]
+  if (month.length === 2 && month[0] === '0') {
+    month = month[1]
+  }
+  let day = date.split('-')[2]
+  if (day.length === 2 && day[0] === '0') {
+    day = day[1]
+  }
+  return `${year}-${month}-${day}`
 }
 
 export default function Home() {
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(getToday())
 
   useEffect(() => {
-    const formattedDate = formatDate(date);
-    getScoreboardData({ date: formattedDate });
-  }, [date]);
+    getScoreboardData({ date: getToday() })
+  }, [])
 
   function handleDateChange(event) {
-    setDate(event.target.value);
+    setDate(event.target.value)
+    getScoreboardData({ date: formatDate(event.target.value) })
   }
 
   return (
     <main>
       <input value={date} type="date" onChange={handleDateChange}></input>
     </main>
-  );
+  )
 }
